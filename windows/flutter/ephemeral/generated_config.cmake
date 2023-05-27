314eeb60635c33a8e57d08f34743da42c9475747
Ahmet (Alin) Tekin <74686688+alinahmettekin@users.noreply.github.com>
2023-05-27 22:43:33 +0300

    connection to firebase

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutternew\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\alinahmettekin\\Desktop\\son\\Blue" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutternew\\flutter"
+  "PROJECT_DIR=C:\\Users\\alinahmettekin\\Desktop\\son\\Blue"
+  "FLUTTER_ROOT=C:\\src\\flutternew\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\alinahmettekin\\Desktop\\son\\Blue\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\alinahmettekin\\Desktop\\son\\Blue"
+  "FLUTTER_TARGET=C:\\Users\\alinahmettekin\\Desktop\\son\\Blue\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\alinahmettekin\\Desktop\\son\\Blue\\.dart_tool\\package_config.json"
+)
